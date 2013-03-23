91c8defef445f5e73b96a37c38b075722599ca27
OHASHI Hideya <ohachige@gmail.com>
2013-03-23 12:08:17 +0900

    add prev hash

--- hashchain/reader.rb
+++ hashchain/reader.rb
@@ -8,13 +8,14 @@ Dir.glob('public/*') do |fn|
   json = File.open(fn, 'rb') do |fd|
     JSON.parse(fd.read)
   end
-  secret_cur, message, fn_next, proof_of_work = json
+  secret_cur, message, fn_prev, fn_next, proof_of_work = json
   difficulty = 4
   raise fn unless Digest::MD5.hexdigest(secret_cur) == bn
   raise fn unless Digest::MD5.hexdigest(json.join('')).index('0' * difficulty) == 0
   messages[bn] = json
 end
 
-messages.each do |id, msg|
-  puts "%s %s %s" % [id[0, 6], msg[2][0, 6], msg[1]]
+messages.each do |id, json|
+  secret_cur, message, fn_prev, fn_next, proof_of_work = json
+  puts "%s %s %s" % [id[0, 6], fn_next[0, 6], message]
 end
--- hashchain/writer.rb
+++ hashchain/writer.rb
@@ -6,15 +6,16 @@ require 'json'
 FileUtils.mkdir_p('public')
 FileUtils.mkdir_p('secret')
 
-prev_fn = ARGV[1]
+fn_prev = ARGV[1]
 secret_cur = nil
 secret_next = nil
 fn_cur = nil
 fn_next = nil
-if prev_fn
-  prev = File.open(prev_fn, 'rb') do |fd|
+if fn_prev
+  prev = File.open(fn_prev, 'rb') do |fd|
     JSON.parse(fd.read)
   end
+  fn_prev = File.basename(fn_prev)
   fn_cur = prev[2]
   cur = File.open(File.join('secret', fn_cur), 'rb') do |fd|
     JSON.parse(fd.read)
@@ -23,6 +24,7 @@ if prev_fn
 else
   secret_cur = rand.to_s
   fn_cur = Digest::MD5.hexdigest(secret_cur)
+  puts "Your ID is %s" % fn_cur
 end
 path = File.join('public', fn_cur)
 raise 'already exist' if File.exist?(path)
@@ -30,17 +32,16 @@ secret_next = rand.to_s
 fn_next = Digest::MD5.hexdigest(secret_next)
 message = ARGV[0] || 'no message'
 message = message.to_json
-json = [secret_cur, message, fn_next]
+json = [secret_cur, message, fn_prev, fn_next]
 difficulty = 4
 zeros = '0' * difficulty
 proof_of_work = nil
 loop do
-  json[3] = rand.to_s
+  json[4] = rand.to_s
   str = Digest::MD5.hexdigest(json.join(''))
   break if str.index(zeros) == 0
 end
-proof_of_work = json[3]
-json = [secret_cur, message, fn_next, proof_of_work]
+proof_of_work = json[4]
 File.open(path, 'wb') do |fd|
   fd.puts(json.to_json)
 end
